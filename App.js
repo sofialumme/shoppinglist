5423c7a05d2560def771309c288fc26185c1f5de
Sofia Lumme <sofia.lumme@gmail.com>
2023-01-26 23:35:02 +0200

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [listItem, setListItem] = useState('');
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const addListItem = () => {
+    setShoppingList([...shoppingList, { text: listItem }]);
+    setListItem('');
+  }
+
+  const clearShoppingList = () => {
+    setShoppingList([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={listItem => setListItem(listItem)}
+        value={listItem}
+      />
+      <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+        <Button title='Add' onPress={addListItem} />
+        <Button title='Clear' onPress={clearShoppingList} />
+      </View>
+      <Text style={{ color: 'blue', fontSize: 20, marginBottom: 5 }}>Shopping List</Text>
+      <FlatList
+        data={shoppingList}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) =>
+          <Text>{item.text}</Text>
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  input: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10
+  }
 });
